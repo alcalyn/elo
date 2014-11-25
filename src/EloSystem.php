1aea74e396ab0d29ea9f94f05e86932eed200fae
Julien Maulny <doubjulien@hotmail.fr>
2014-11-25 13:40:30 +0100

    doc functions

--- src/EloSystem.php
+++ src/EloSystem.php
@@ -45,24 +45,24 @@ class EloSystem
     /**
      * Calculate new elo scores
      * 
-     * @param float $elo0 elo score of player 0
-     * @param float $elo1 elo score of player 1
-     * @param float $win win coef for player 0. Set 1 to say that player 0 won
-     * @param float $reliability0 elo reliability for player 0
-     * @param float $reliability1 elo reliability for player 1
+     * @param double $elo0 elo score of player 0
+     * @param double $elo1 elo score of player 1
+     * @param double $win win coef for player 0. Set 1 to say that player 0 won
+     * @param double $reliability0 elo reliability for player 0
+     * @param double $reliability1 elo reliability for player 1
      * @param integer $kFactor0 override k factor for player 0
      * @param integer $kFactor1 override k factor for player 1
      * 
-     * @return array with new elo score at indexes 0 and 1 for player 0 and 1
+     * @return double[] with new elo score at indexes 0 and 1 for player 0 and 1
      */
-    public function calculate($elo0, $elo1, $win, $reliability0 = 1, $reliability1 = 1, $kFactor0 = -1, $kFactor1 = -1)
+    public function calculate($elo0, $elo1, $win, $reliability0 = 1.0, $reliability1 = 1.0, $kFactor0 = null, $kFactor1 = null)
     {
         self::checkCoef($reliability0, 'reliability0');
         self::checkCoef($reliability1, 'reliability1');
         self::checkCoef($win, 'win');
         
-        $kFactor0 = ($kFactor0 < 0) ? $this->kFactor : $kFactor0;
-        $kFactor1 = ($kFactor1 < 0) ? $this->kFactor : $kFactor1;
+        $kFactor0 = (null === $kFactor0) ? $this->kFactor : $kFactor0;
+        $kFactor1 = (null === $kFactor1) ? $this->kFactor : $kFactor1;
         
         /**
          * Calculate probability 0 have to beat 1
@@ -96,12 +96,12 @@ class EloSystem
     /**
      * Player 0 beat player 1
      * 
-     * @param float $elo0 elo score of player 0
-     * @param float $elo1 elo score of player 1
-     * @param float $reliability0 elo reliability for player 0
-     * @param float $reliability1 elo reliability for player 1
+     * @param double $elo0 elo score of player 0
+     * @param double $elo1 elo score of player 1
+     * @param double $reliability0 elo reliability for player 0
+     * @param double $reliability1 elo reliability for player 1
      * 
-     * @return array with new elo score at indexes 0 and 1 for player 0 and 1
+     * @return double[] with new elo score at indexes 0 and 1 for player 0 and 1
      */
     public function win($elo0, $elo1, $reliability0 = 1, $reliability1 = 1)
     {
@@ -111,12 +111,12 @@ class EloSystem
     /**
      * Player 0 lose against player 1
      * 
-     * @param float $elo0 elo score of player 0
-     * @param float $elo1 elo score of player 1
-     * @param float $reliability0 elo reliability for player 0
-     * @param float $reliability1 elo reliability for player 1
+     * @param double $elo0 elo score of player 0
+     * @param double $elo1 elo score of player 1
+     * @param double $reliability0 elo reliability for player 0
+     * @param double $reliability1 elo reliability for player 1
      * 
-     * @return array with new elo score at indexes 0 and 1 for player 0 and 1
+     * @return double[] with new elo score at indexes 0 and 1 for player 0 and 1
      */
     public function lose($elo0, $elo1, $reliability0 = 1, $reliability1 = 1)
     {
@@ -126,12 +126,12 @@ class EloSystem
     /**
      * Player 0 and player 1 have made a draw game
      * 
-     * @param float $elo0 elo score of player 0
-     * @param float $elo1 elo score of player 1
-     * @param float $reliability0 elo reliability for player 0
-     * @param float $reliability1 elo reliability for player 1
+     * @param double $elo0 elo score of player 0
+     * @param double $elo1 elo score of player 1
+     * @param double $reliability0 elo reliability for player 0
+     * @param double $reliability1 elo reliability for player 1
      * 
-     * @return array with new elo score at indexes 0 and 1 for player 0 and 1
+     * @return double[] with new elo score at indexes 0 and 1 for player 0 and 1
      */
     public function draw($elo0, $elo1, $reliability0 = 1, $reliability1 = 1)
     {
@@ -141,12 +141,12 @@ class EloSystem
     /**
      * Return probability rate that $elo0 beat $elo1
      * 
-     * @param float $elo0
-     * @param float $elo1
+     * @param double $elo0
+     * @param double $elo1
      * @param integer $interval
      * @param integer $pow
      * 
-     * @return float
+     * @return double
      */
     public static function proba($elo0, $elo1, $interval = 400, $pow = 10)
     {
@@ -156,9 +156,8 @@ class EloSystem
     /**
      * Check if $coef is in range [0;1]
      * 
-     * @param float $coef
-     * 
-     * @return boolean
+     * @param double $coef
+     * @param string $variableName
      */
     private static function checkCoef($coef, $variableName)
     {
